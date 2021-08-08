d69c4dbc3bd9c4366c545962a56d7a901d9d243d
protas9534 <protas9534@icloud.com>
2021-08-08 10:38:55 +0300

    add registration and entry form

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,76 +1,79 @@
 "use strict";
 
-// WINDOW WIDTH DETECT
-var currentWidth = $(window).width();
-$(window).on('resize', function () {
-  currentWidth = $(window).width();
-}); //App
+var registration = document.querySelector('.reg__registration');
 
-var Application = {
-  init: function init() {
-    this.events();
-  },
-  events: function events() {
-    !window.reinit && (window.reinit = {}); //main events
-
-    this.eventList.lazyImgs();
-    this.eventList.svgPolifill();
-    this.eventList.mask(); //app events
-    //this.eventList.sliders();
-  },
-  eventList: {
-    lazyImgs: function lazyImgs() {
-      var callback_loaded = function callback_loaded(el) {
-        var $img = $(el),
-            $img_wrap = $img.closest('.lazy-img-wrap');
-        $img_wrap.addClass('loaded');
-      };
-
-      new LazyLoad({
-        elements_selector: ".lazy-img",
-        threshold: 0,
-        callback_loaded: callback_loaded
-      });
-    },
-    svgPolifill: function svgPolifill() {
-      // svg sprites
-      svg4everybody && svg4everybody();
-    },
-    mask: function mask() {
-      $("[type=tel]").inputmask({
-        mask: '+7 (999) 99-99-99',
-        "clearIncomplete": false,
-        "showMaskOnHover": false,
-        "showMaskOnFocus": true,
-        "clearMaskOnLostFocus": true
-      });
-    },
-    sliders: function sliders() {
-      $('.js-swiper-banner').each(function (i) {
-        var bannerSwiper = new Swiper($('.js-swiper-banner .swiper-container')[i], {
-          // Disable preloading of all images
-          //preloadImages: false,
-          // Enable lazy loading
-          //lazy: {
-          //    loadPrevNext: true,
-          //},
-          autoplay: {
-            delay: 5000
-          },
-          loop: true,
-          spaceBetween: 15,
-          // Navigation arrows
-          navigation: {
-            nextEl: $('.js-swiper-banner .swiper-button-next')[i],
-            prevEl: $('.js-swiper-banner .swiper-button-prev')[i]
-          }
-        });
-      });
-    }
-  }
+var regBlock = function regBlock() {
+  document.querySelector('.reg__window-entry').style.display = 'none';
+  document.querySelector('.reg__window-reg').style.display = 'flex';
 };
-$(function () {
-  'use strict';
 
-  Application.init();
-});
+registration.addEventListener('click', regBlock);
+var entry = document.querySelector('.reg__entry-reg');
+
+var entryBlock = function entryBlock() {
+  document.querySelector('.reg__window-entry').style.display = 'flex';
+  document.querySelector('.reg__window-reg').style.display = 'none';
+};
+
+entry.addEventListener('click', entryBlock);
+var users = [];
+var formReg = document.querySelector('.reg__form-reg');
+
+formReg.onsubmit = function (event) {
+  event.preventDefault();
+  var email = document.querySelector('.reg__email-input-reg').value;
+  var tel = document.querySelector('.reg__tel-input-reg').value;
+  var pass1 = document.querySelector('.reg__pass-input-reg').value;
+  var pass2 = document.querySelector('.pass').value;
+
+  var checkPass = function checkPass() {
+    if (pass1 === pass2) {
+      return pass1;
+    } else {
+      document.querySelector('.reg__err').style.opacity = '1';
+    }
+  };
+
+  var person = {
+    email: email,
+    tel: tel,
+    pass: checkPass()
+  };
+
+  var pushUser = function pushUser() {
+    if (!!person.pass == true) {
+      users.push(person);
+      entryBlock();
+      console.log(users);
+    }
+  };
+
+  pushUser();
+};
+
+var form = document.querySelector('.reg__form');
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+  var email = document.querySelector('.reg__email-input').value;
+  var pass = document.querySelector('.reg__pass-input').value;
+  console.log(email + ' ' + pass);
+  var allPass = users.map(function (person) {
+    return person.pass;
+  });
+  var allEmail = users.map(function (person) {
+    return person.email;
+  });
+
+  var checkUser = function checkUser() {
+    if (allEmail.includes(email) && allPass.includes(pass)) {
+      document.querySelector('.registration').style.display = 'none';
+    } else {
+      document.querySelector('.reg__pass-err').style.opacity = '1';
+    }
+  };
+
+  checkUser();
+};
+
+console.log(users);
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,83 +1,71 @@
-// WINDOW WIDTH DETECT
-let currentWidth = $(window).width();
-$(window).on('resize', function () {
-    currentWidth = $(window).width();
-});
+let registration = document.querySelector('.reg__registration')
+let regBlock = () => {
+    document.querySelector('.reg__window-entry').style.display = 'none'
+    document.querySelector('.reg__window-reg').style.display = 'flex'
+}
+registration.addEventListener('click', regBlock)
 
-//App
-const Application = {
-    init: function() {
-        this.events();
-    },
-    events: function() {
-        !window.reinit && (window.reinit = {});
+let entry = document.querySelector('.reg__entry-reg')
+let entryBlock = () => {
+    document.querySelector('.reg__window-entry').style.display = 'flex'
+    document.querySelector('.reg__window-reg').style.display = 'none'
+}
+entry.addEventListener('click', entryBlock)
 
-        //main events
-        this.eventList.lazyImgs();
-        this.eventList.svgPolifill();
-        this.eventList.mask();
 
-        //app events
-        //this.eventList.sliders();
-    },
-    eventList: {
-        lazyImgs: function(){
 
-            let callback_loaded = function(el) {
-                const $img = $(el),
-                      $img_wrap = $img.closest('.lazy-img-wrap');
-                $img_wrap.addClass('loaded');
-            };
+let users = []
 
-            new LazyLoad({
-                elements_selector: ".lazy-img",
-                threshold: 0,
-                callback_loaded: callback_loaded,
-            });
-        },
-        svgPolifill: function () {
-            // svg sprites
-            svg4everybody && svg4everybody();
-        },
-        mask: function() {
-            $("[type=tel]").inputmask({
-                mask: '+7 (999) 99-99-99',
-                "clearIncomplete": false,
-                "showMaskOnHover": false,
-                "showMaskOnFocus": true,
-                "clearMaskOnLostFocus": true
-            });
-        },
-
-        sliders: function(){
-            $('.js-swiper-banner').each(function(i){
-                let bannerSwiper = new Swiper($('.js-swiper-banner .swiper-container')[i], {
-                    // Disable preloading of all images
-                    //preloadImages: false,
-                    // Enable lazy loading
-                    //lazy: {
-                    //    loadPrevNext: true,
-                    //},
-                    autoplay: {
-                        delay: 5000,
-                    },
-                    loop: true,
-                    spaceBetween: 15,
-
-                    // Navigation arrows
-                    navigation: {
-                        nextEl: $('.js-swiper-banner .swiper-button-next')[i],
-                        prevEl: $('.js-swiper-banner .swiper-button-prev')[i],
-                    },
-                });
-            });
-            
-        },
+let formReg = document.querySelector('.reg__form-reg')
+formReg.onsubmit = (event) => {
+    event.preventDefault()
+    let email = document.querySelector('.reg__email-input-reg').value
+    let tel = document.querySelector('.reg__tel-input-reg').value
+    let pass1 = document.querySelector('.reg__pass-input-reg').value
+    let pass2 = document.querySelector('.pass').value
+    let checkPass = () => {
+        if (pass1 === pass2){
+        return pass1
+        } else {
+        document.querySelector('.reg__err').style.opacity = '1'
+        }
     }
-};
-$(function () {
-    'use strict';
-    Application.init();
-});
+    let person = {
+        email: email,
+        tel: tel,
+        pass: checkPass()
+    }
+    
+    let pushUser = () => {
+        if (!!person.pass == true){
+            users.push(person)
+            entryBlock()
+            console.log(users)
+        }
+    }
+    pushUser()
+}
 
+
+
+
+
+let form = document.querySelector('.reg__form')
+form.onsubmit = (event) => {
+    event.preventDefault()
+    let email = document.querySelector('.reg__email-input').value
+    let pass = document.querySelector('.reg__pass-input').value
+    console.log(email + ' ' + pass)
+    let allPass = users.map(person => person.pass)
+    let allEmail = users.map(person => person.email)
+    let checkUser = () => {
+        if (allEmail.includes(email) && allPass.includes(pass)){
+            document.querySelector('.registration').style.display = 'none'
+        } else {
+            document.querySelector('.reg__pass-err').style.opacity = '1'
+        }
+    }
+    checkUser()
+}
+console.log(users)
 
